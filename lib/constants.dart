b2769d7959e4ee194d43823e5e9afc96eb8bd6f5
AndrewAdell <andrewadel3333@gmail.com>
2024-05-22 19:16:11 +0200

    adding gtsectra font&edite style class

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const keyPrimaryColor = Color(0xff100B20);
 const kTranstionDuration = Duration(milliseconds: 250);
+const gtSectraFineFont = 'GT Sectra Fine';
