c7f9cb01ef5caaf6d04513793d0b064e66033c02
AndrewAdell <andrewadel3333@gmail.com>
2024-05-24 23:04:10 +0200

    add title&subtitle to book details

--- lib/features/Home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/Home/presentation/view/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/Home/presentation/view/widgets/book_details_app_bar.dart';
 import 'package:bookly/features/Home/presentation/view/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +13,26 @@ class BookDetailsViewBody extends StatelessWidget {
       children: [
         const BookDetailsAppBar(),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .28),
+          padding: EdgeInsets.symmetric(horizontal: width * .2),
           child: const CustomBookItem(),
-        )
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        Text(
+          'the jungle book',
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Rudyard kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
       ],
     );
   }
